2cb4d87db57c91a83334212d2b9f8cbd3931a1d4
Roman Savenkov <cybersportsmen1@mail.ru>
2024-11-16 01:07:57 +0400

    refac

--- js.js
+++ js.js
@@ -1,52 +1,44 @@
-window.addEventListener("DOMContentLoaded", function () {
-  let showButton = document.querySelector(".show-all-btn");
-  let container = document.querySelector(".swiper-wrapper");
-  let swiper;
+window.addEventListener('DOMContentLoaded', function () {
+  let showButton = document.querySelector('.show-all-btn')
+  let container = document.querySelector('.swiper-wrapper')
+  let swiper
 
   function initSwiper() {
-    swiper = new Swiper(".swiper", {
-      direction: "horizontal",
-      slidesPerView: "auto",
+    swiper = new Swiper('.swiper', {
+      direction: 'horizontal',
+      slidesPerView: 'auto',
 
       loop: true,
 
       pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
+        el: '.swiper-pagination',
+        clickable: true
+      }
+    })
   }
-  const breakpoint = window.matchMedia("(min-width: 768px)");
+  const breakpoint = window.matchMedia('(min-width: 768px)')
   function destroySwiper() {
     if (swiper) {
-      swiper.destroy();
-      swiper = null;
+      swiper.destroy()
     }
   }
+
   function handleResize() {
     if (breakpoint.matches) {
-      destroySwiper();
+      destroySwiper()
     } else {
-      initSwiper();
+      initSwiper()
     }
   }
-  handleResize();
+  handleResize()
 
-  breakpoint.addEventListener("change", handleResize);
+  breakpoint.addEventListener('change', handleResize)
 
-  showButton.addEventListener("click", function () {
-    if (showButton.classList.contains("show-all--rotate")) {
-      showButton.classList.remove("show-all--rotate");
-      container.classList.add("swiper--opened");
-      container.classList.add("swiper-wrapper--margin-bottom");
-      showButton.classList.add("show-less--rotate");
-      showButton.textContent = "Скрыть";
-    } else {
-      showButton.classList.add("show-all--rotate");
-      container.classList.remove("swiper--opened");
-      container.classList.remove("swiper-wrapper--margin-bottom");
-      showButton.classList.remove("show-less--rotate");
-      showButton.textContent = "Показать все";
-    }
-  });
-});
+  showButton.addEventListener('click', function () {
+    this.textContent = this.classList.contains('show-all--rotate') ? 'Скрыть' : 'Показать все'
+    this.classList.toggle('show-all--rotate')
+    container.classList.toggle('swiper--opened')
+    container.classList.toggle('swiper-wrapper--margin-bottom')
+    this.classList.toggle('show-less--rotate')
+  })
+})
